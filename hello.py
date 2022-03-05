7cab108f415a2acad47b09c6ab43d419866c3b80
Md. Sayed Ahammed <sayed.ahammed@reddotdigitalit.com>
2022-03-05 18:05:42 +0600

    first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = "Rebeka Sultana"
+print(name)
+
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
---
+++ py-string.py
@@ -0,0 +1,18 @@
+# title
+name = "ada lovelace"
+print(name.title())
+
+# upper
+print(name.upper())
+
+# lower
+print(name.lower())
+
+# Concatenating
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
